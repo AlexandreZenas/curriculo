82435ea1379160387f68f9a7067fa8d63e90703c
alexandrezenas <alexandrezenas11@gmail.com>
2022-05-27 00:42:52 -0300

    teste

---
+++ portifólio.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>portifólio</title>
+<!-- MetaTags-->
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width,inital-scale=1.0,maximun-scale=1.0">
+   <meta name="keywords" content="">
+<!--links-->
+   <link rel="stylesheet" type="text/css" href="./css/portifólio.CSS" >
+</head >
+<body>
+
+<!--section 01-->
+   <section class="section01">
+   <!--overlay-->
+      <div class="overlay"></div>
+   <!--container 01-->
+      <div class="container01">
+         <div class="h1_a1"> 
+            <div class="h1_a">
+               <h2> A </h2>
+            </div>
+         </div>
+         <div>
+            <h1>Alexandre Zenas</h1>
+         </div>
+         <div>
+            <p>
+               Desenvolvedor Web || Dev Front-end 
+            </p>
+         </div>
+      </div>
+      <!--clear-->
+      <div class="clear"></div>
+   </section>
+
+<!--section 02-->
+   <section class="section02">
+      <!--container 03-->
+      <div class="containerS201">
+         <div class="box_foto_na_sala">
+            <img src="./fotos/foto_ano_novo.jpg">
+         </div>
+         <div class="box_informações">
+            <div class="line-text">
+               <div></div>
+            </div><br>
+            
+            <h5>Idade: 19 anos</h5>
+            <h5>Estado civil: Solteiro</h5>
+            <h5>endereço: Guaratiba/RJ</h5>            
+            <h5>email:<a href="mailto:alexandrezenas11@gmail.com"> alexandrezenas11@gmail.com</a></h5>
+            
+            <a href="mailto:alexandrezenas11@gmail.com"><i class="fas fa-external-link"></i></a>
+            <a href="https://www.linkedin.com/in/alexandre-zenas-819b3163/" target="_blank"><i class="fab fa-linkedin"></i></a>
+            <a href="https://github.com/AlexandreZenas"><i class="fab fa-github" target="_blank"></i></a>
+            <a href="https://www.instagram.com/alexandrezenas/" target="_blank"><i class="fab fa-instagram"></i></a>
+         </div>
+         <!--clear-->
+         <div class="clear"></div>
+      </div>
+      <!--container 02-->
+         <div class="containerS202" style="">
+            <i class="fas fa-bars bars2" ></i>
+            <!--conteudo-->
+               <div class="conteudo">
+                  <!--parte 01-->
+                     <i class="fal fa-bookmark"></i><br>
+                     <div class="line-text">
+                        <div></div>
+                     </div><br>
+                     <h2> Objetivo  </h2><br>            
+                     <p> Adquirir experiência e me profissionalizar em desenvolvimento web, na área de front-end.<br></p>
+                     <h2> Experiência profissional </h2> <br>          
+                     <h6>Estágio de ensino médio | Caixa Econômica Federal </h6>
+                     <p>Atuei na parte de administração fazendo a organização de arquivos, documentos e dossiês, a elaboração de planilhas, a confecção de relatórios, o preenchimento de formulários internos e prestando suporte a gerência. No setor vendas, prospectando clientes inbound. Na recepção, e no atendimento ao cliente.
+                     </p>
+                     <h2>Idiomas </h2> <br>          
+                     <h6> Inglês intermediário/avançado</h6>
+                     <h6> Espanhol básico</h6>
+                     <h2> Informações complementares  </h2><br>  
+                     <h6>Atualmente meu foco está em progamar na web, usando HTML e CSS. Tenho o JavaScript como a principal linguagem de progamação.</h6>          
+                     <h6>Noções de Banco de Dados e MySql.</h6>
+                     <h6> Colaborei na manutenção do website <a href="https://www.mscleaner.com.br/" target="_blank">mscleanner,</a> empresa de limpeza e manutenção de pisos e conserto subaquáticos de piscinas.<br></h6>
+                     <h6>Experiência com montagem e manutenção de computadores.</h6>
+                     
+                     
+                  <!--parte 02-->   
+                     <!--<i class="fal fa-baseball-ball"></i><br>
+                     <div class="line-text">
+                        <div></div>
+                     </div>
+                     <h3>Hobbies</h3><br>
+                     <p>Tecnologia | Música | Finanças | Jogos | Animes | Volêi</p>-->
+
+
+                                         
+               </div>            
+         </div>
+   </section>
+<!--section 03-->
+   <section class="section03">
+      <div class="containerS301">
+            <div class="S3parte01">
+               <i class="fal fa-lightbulb-on"></i><br><BR><br>
+            </div>
+            <div class="S3parte02">
+               <h2>Formação</h2><BR>
+
+               <h4>Centro Universitário IBMR | Barra </h4><BR>
+               <h5>Curso: Análise e Desenvolvimento de sistemas | Tecnólogo </h5>
+               <p>Atualmente com a matrícula trancada no 3º período com a previsão de término para o 2º semestre de 2023.</p>
+               <br> <br>
+            </div>
+            <div class="S3parte02">
+               <h2>Cursos</h2><BR>
+
+               <h4>Danki.Code</h4><BR>
+               <h5>Curso: full-stack master | Curso Online</h5>
+               <p>Plataforma de estudos com foco em desenvolvimento Front/Back-end e mobile.</p>
+               <br> <br>
+
+               <h4>Rocketseat</h4><BR>
+               <h5>Curso: Next Level Week - Origin | Curso Online</h5>    
+               <p>Conteúdo também direcionado para a área de desenvolvimento Web.</p>           
+               <i class="fas fa-chalkboard-teacher"></i><br>
+                     <div class="line-text line03">
+                        <div></div>
+                        <!--clear-->
+                        <div class="clear"></div>
+                     </div>
+               <h4 class="redes-sociais"> Redes Sociais</h4>
+                  <p id="socialLinks"class="block">
+                     <a class="linkedin" href="https://www.linkedin.com/in/alexandre-zenas-819b3163/" target="_blank"><i class="fab fa-linkedin">  : Alexandre Zenas |</i></a>   
+                     <a class="github" href="https://github.com/AlexandreZenas"><i class="fab fa-github" target="_blank">  : AlexandreZenas |</i></a>
+                     <a class="instagram" href="https://www.instagram.com/alexandrezenas/" target="_blank"><i class="fab fa-instagram">  : alexandrezenas</i></a>
+                  
+                  </p>
+               
+               
+            </div>
+      </div>
+      <div class="containerS302">
+         <div class="S3parte03">
+            <i class="fal fa-star"></i><br>
+            <div class="line-text"><div></div></div><br><br><br>
+            <h2>Skills</h2>
+         </div>
+         <div class="S3parte04">
+            <h2>Linguagens<br>
+               <div class="line-text line-textS3 "><div></div></div>
+            </h2>               
+            <div class="div_line">
+               <p>HTML</p> <p class="porcentagem">85%</p>
+               <div class="line-text lineS3"><div class="line_One"></div></div><br><br><br>
+            </div>
+            <div class="div_line">
+               <p>CSS</p><p class="porcentagem">65%</p>
+               <div class="line-text lineS3 "><div class="line_Two"></div></div><br><br><br>
+            </div>
+            <div class="div_line">
+               <p>JavaScript</p><p class="porcentagem">45%</p>
+               <div class="line-text lineS3"><div class="line_Three"></div></div><br><br><br>
+            </div>
+         </div>
+         <div class="S3parte05 sinais">
+            <i class="fal fa-less-than"></i>
+            <i class="fal fa-greater-than"></i>
+         </div>
+      </div>
+   </section>
+
+   
+   
+   <!--<script>
+      const LinksSocialMedia = {
+         linkedin: 'in/alexandre-zenas-819b3163/',
+         github: 'AlexandreZenas',
+         instagram: 'alexandrezenas'
+      }
+
+      function changeSocialMediaLinks() {
+         for (let li of socialLinks.children){
+            const social = li.getAttribute('class')
+
+            li.href = `https://${social}.com/${LinksSocialMedia[social]}`
+            alert(li.children[0].href)
+         }
+      }
+      changeSocialMediaLinks()
+
+   </script>-->
+	<script type="text/javascript" src="./JavaScript/arquivo_de_estudo.js"></script>
+</body>
+</html>
